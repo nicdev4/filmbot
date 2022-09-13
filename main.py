72cf9403821810171031416c7f05bc6281889898
37529 <valf2k3@gmail.com>
2022-09-13 15:30:26 +0300

    Система для замены Эмоджи по ключевым словам.

--- main.py
+++ main.py
@@ -3,10 +3,27 @@ import telebot
 token = "5775482246:AAFoL2J2e2MGUBNvkkwE28vScOcmtIW7XV4"
 
 bot = telebot.TeleBot(token=token)
+types = telebot.types;
 
+admins = [831107251]
+
+emojies = {
+    ':heart:': '❤',
+    ':makaka:': '🙊',
+}
+
+def replaceEmojies(message):
+    for key in emojies:
+        message = str(message).replace(key,emojies[key])
+    return message;
 @bot.message_handler()
 def reply(message):
     id = message.chat.id
-    bot.send_message(id, "test")
+    if(admins.__contains__(id)):
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton("Open google", url="https://google.com"))
+        bot.send_message(id, replaceEmojies(":heart: :makaka: test"), reply_markup=markup)
+    else:
+        bot.send_message(id, "Бот временно недоступен ;(")
 
 bot.infinity_polling()
